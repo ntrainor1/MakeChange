59a9a43a041ead3f53af31e299d906b2f6613253
Nathaniel Trainor <natetrainor@msn.com>
2018-03-02 18:12:54 -0700

    Completed User Story #3

--- src/com/skilldistillery/projects/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/projects/makechange/MakeChangeApp.java
@@ -6,15 +6,39 @@ public class MakeChangeApp {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		
+
 		System.out.println("Hello there, stranger.");
 		System.out.print("What is the cost of your product? (in dollars, e.g. 1.20) ");
 		double price = keyboard.nextDouble();
-		
+
 		System.out.print("I see. Give me some payment: (in dollars, e.g. 2.75) ");
-		double payment = keyboard.nextDouble();
-		
+
+		while (true) {
+			double payment = keyboard.nextDouble();
+
+			if (payment == price) {
+				thankYou();
+				break;
+			}
+			else if (payment <= price) {
+				insufficientPayment();
+			}
+			else {
+				break;
+			}
+
+		}
+
+		System.out.println("Have a nice day!");
 		keyboard.close();
+	}
+
+	private static void insufficientPayment() {
+		System.out.print("Your payment falls short. Please insert the correct change. ");
+	}
+
+	private static void thankYou() {
+		System.out.println("Thank you for using exact change.");
 	}
 
 }
