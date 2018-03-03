cfbb7989217b2e83e78a89b590311a9fcf1dcbd5
Nathaniel Trainor <natetrainor@msn.com>
2018-03-02 17:57:58 -0700

    Completed User Story #1

--- src/com/skilldistillery/projects/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/projects/makechange/MakeChangeApp.java
@@ -1,10 +1,18 @@
 package com.skilldistillery.projects.makechange;
 
+import java.util.Scanner;
+
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Hello there, stranger.");
+		System.out.print("What is the cost of your product? (in dollars, e.g. 1.20) ");
+		double price = keyboard.nextDouble();
+		
+		System.out.print("I see. Give me some payment: (in dollars, e.g. 2.75) ");
+		
 	}
 
 }
